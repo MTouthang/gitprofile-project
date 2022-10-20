3b0052b8380e44cdd75b397a0bb9fa155aeb0686
Mamang Touthang <huebart16@gmail.com>
2022-10-20 19:15:53 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+// react-router
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+// toast
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
+// components
+import Home from "./Pages/Home";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import PageNotFound from "./Pages/PageNotFound";
+import { UserContext } from "./context/UserContext";
+import Footer from "./layout/Footer";
+import Header from "./layout/Header";
+
+import firebaseConfig from "./config/firebaseConfig";
+
+// init firebase -
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ToastContainer />
+      <UserContext.Provider value={{ user, setUser }}>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/signin" element={<Signin />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </UserContext.Provider>
+    </Router>
   );
 }
 
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBJe29WfZBdm-u8FVSzuuzmw_73UrN1ua0",
+  authDomain: "gitprofile-react.firebaseapp.com",
+  projectId: "gitprofile-react",
+  storageBucket: "gitprofile-react.appspot.com",
+  messagingSenderId: "650643365333",
+  appId: "1:650643365333:web:8ff9afdd67e399e0cbb15c",
+  measurementId: "G-0HXBZHZVQC",
+};
+
+export default firebaseConfig;
